c1a581d006ffef320b076c380d41d11bef614e83
Yash Gandhi <yashgandhi303@gmail.com>
2022-05-04 06:26:36 +0530

    mui, eslint, prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
+import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
